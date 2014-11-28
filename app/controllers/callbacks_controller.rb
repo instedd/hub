a1e79a1297ff2b1d10a215326786ae7d9275c1d0
Carolina Hadad <chadad@manas.com.ar>
2014-11-28 18:19:23 -0300

    Try to parse the received JSON. If it is invalid it could not be processed by the worker. Pending: Add better error handling and tests. Just trying to setup the environment for ACT project now.

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -11,6 +11,10 @@ class CallbacksController < ApplicationController
 
   def enqueue
     raw_post = request.raw_post.empty? ? "{}" : request.raw_post
+
+    # This will raise an exception if the json is invalid
+    JSON.parse raw_post
+
     connector.enqueue_event(params[:event], raw_post)
     render json: {}, status: :ok
   end
