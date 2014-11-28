8e292e52468b88c01cea6cd898b3091fa2efdc67
Juan Wajnerman <jwajnerman@manas.com.ar>
2014-11-28 17:43:17 -0300

    Refactor Google Spreadsheets connector to use default actions

--- app/models/google_spreadsheets_connector.rb
+++ app/models/google_spreadsheets_connector.rb
@@ -7,15 +7,19 @@ class GoogleSpreadsheetsConnector < Connector
   validates_presence_of :refresh_token
   validates_presence_of :expires_at
 
+  def label
+    "Spreadsheets"
+  end
+
   def path
     ""
   end
 
   def query(filters, user, options)
-    [Spreadsheet.new(self, spreadsheet_key, spreadsheet_name)]
+    [Spreadsheet.new(self, spreadsheet_key)]
   end
 
-  def find_entity(key)
+  def find_entity(key, user)
     Spreadsheet.new(self, key)
   end
 
@@ -90,89 +94,143 @@ class GoogleSpreadsheetsConnector < Connector
   end
 
   class Spreadsheet
-    include Entity
+    include EntitySet
 
-    attr_reader :parent
     attr_reader :key
 
-    def initialize(parent, key, name = nil)
+    def initialize(parent, key)
       @parent = parent
       @key = key
-      @name = name
     end
 
     def label
-      @name
+      connector.spreadsheet_name
     end
 
     def path
       @key
     end
 
-    def actions(user)
-      {"insert_row" => InsertRow.new(self)}
+    def spreadsheet
+      session = GoogleDrive.login_with_oauth(connector.access_token)
+      session.spreadsheet_by_key(@key)
+    end
+
+    def query(filter, user, options)
+      spreadsheet.worksheets.map do |ws|
+        Worksheet.new(self, ws.title, ws)
+      end
+    end
+
+    def find_entity(title, user)
+      Worksheet.new(self, title)
     end
   end
 
-  class InsertRow
-    include Action
+  class Worksheet
+    include EntitySet
+    protocol :update, :insert, :delete
 
-    attr_reader :parent
+    class InsertAction < EntitySet::InsertAction
+      def args(user)
+        super.tap do |args|
+          args[:properties][:type][:open] = true
+        end
+      end
 
-    def initialize(parent)
+      def after_create(binding)
+        define_columns_for binding
+      end
+
+      def after_update(binding)
+        define_columns_for binding
+      end
+
+      def define_columns_for(binding)
+        new_columns = binding["members"]["properties"]["open"].keys
+        worksheet = parent.worksheet
+        list = worksheet.list
+        headers = list.keys
+        new_headers = (headers + new_columns).uniq
+        list.keys = new_headers
+        worksheet.save
+      end
+    end
+
+    def initialize(parent, label, worksheet = nil)
       @parent = parent
+      @label = label
+      @worksheet = worksheet
     end
 
     def label
-      "Insert row"
+      @label
     end
 
-    def sub_path
-      "insert_row"
+    def path
+      "#{@parent.path}/#{@label}"
     end
 
-    def args(user)
-      headers = worksheet.list.keys
-      {
-        properties: {
-          type: {
-            kind: :struct,
-            members: Hash[headers.map { |h| [h, {type: :string}] }],
-            open: true,
-          },
-        }
-      }
+    def reflect_entities(user)
+      # Rows are not displayed during reflection
     end
 
-    def invoke(args, user)
+    def filters
+      headers
+    end
+
+    def entity_properties
+      Hash[headers.map { |h| [h, SimpleProperty.string(h, nil)] }]
+    end
+
+    def query(filters, current_user, options)
+      worksheet.list.select do |row|
+        filters.all? do |key, value|
+          row[key] == value
+        end
+      end.map { |row| Row.new(self, row) }
+    end
+
+    def insert(properties, user)
       worksheet = self.worksheet
       list = worksheet.list
-      list.push args["properties"]
+      list.push properties
       worksheet.save
     end
 
-    def after_create(binding)
-      define_columns_for binding
+    def update(keys, properties, user)
+      raise "not implemented"
     end
 
-    def after_update(binding)
-      define_columns_for binding
+    def delete(keys, user)
+      raise "not implemented"
     end
 
-    def define_columns_for(binding)
-      new_columns = binding["members"]["properties"]["open"].keys
-      worksheet = self.worksheet
-      list = worksheet.list
-      headers = list.keys
-      new_headers = (headers + new_columns).uniq
-      list.keys = new_headers
-      worksheet.save
+    def find_entity(id, user)
+      Row.new(self, worksheet.list[id.to_i - 1])
     end
 
     def worksheet
-      session = GoogleDrive.login_with_oauth(connector.access_token)
-      spreadsheet = session.spreadsheet_by_key(parent.key)
-      spreadsheet.worksheets.first
+      @worksheet ||= begin
+        parent.spreadsheet.worksheet_by_title(@label)
+      end
+    end
+
+    def headers
+      worksheet.list.keys
+    end
+  end
+
+  class Row
+    include Entity
+
+    def initialize(parent, row)
+      @parent = parent
+      @row = row
+    end
+
+    def properties
+      Hash[parent.headers.map { |h| [h, SimpleProperty.string(h, @row[h])] }]
     end
   end
 end
